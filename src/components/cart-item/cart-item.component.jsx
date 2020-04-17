c2e2cc281a43368aeb5113343b303c6d1f7b543d
jessecha <jessecha222@gmail.com>
2020-04-17 14:13:54 +0900

    updating application with reselect, replaced cart selector

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './cart-item.styles.scss'
 
-const CartItem = ({ item: { imageUrl, price, name, quantitiy } }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className="cart-item">
         <img src={imageUrl} alt="photo"/>
         <div className="item-details">
             <span className="name">{name}</span>
             <span className="price">
-                {quantitiy} X {price}
+                {quantity} X {price}
                 </span>
         </div>
     </div>
